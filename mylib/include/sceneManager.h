69c41efa7773d1be6c5665d92c02bf04b7847e14
codestar <glegat@gaming.tech>
2025-02-11 12:58:00 +0100

    [dev] worked on including .dll for users to start the program without sfml installed

--- mylib/include/sceneManager.h
+++ mylib/include/sceneManager.h
@@ -7,10 +7,11 @@
 class SceneManager
 {
 public:
-	SceneManager(const int& width, const int& height, std::string title);
+	SceneManager(const int& width, const int& height, const std::string& title);
 
+	void push_back(std::unique_ptr<SceneBase> scene);
 	sf::RenderWindow* getWindow();
-	SceneBase getCurrentScene();
+	SceneBase* getCurrentScene();
 	void setCurrentScene(const int& index);
 	void processInput();
 	void exec();
@@ -18,4 +19,5 @@ public:
 private:
 	std::unique_ptr<sf::RenderWindow> m_window;
 	std::vector<std::unique_ptr<SceneBase>> m_scenes;
+	SceneBase* m_currentScene;
 };
